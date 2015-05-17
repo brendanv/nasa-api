bb4cdc3bb3d2c19c1158dc2e63e8c42e2cc03185
Brendan Viscomi <matchpoint515@gmail.com>
2015-05-16 18:50:44 -0700

    Switch EarthImagery to extend NasaApiObject

--- nasa/earth.py
+++ nasa/earth.py
@@ -17,48 +17,20 @@ class EarthAsset(NasaApiObject):
     def get_asset_image(self, dim=None):
         # API expects only YYYY-MM-DD
         date = self.date[:10]
-        return self._api.earth_imagery.query(self.lat, self.lon, dim, date)
+        return self._api.get_earth_image(self.lat, self.lon, dim, date)
 
 
-class EarthImagery(object):
+class EarthImagery(NasaApiObject):
     """NASA Earth Imagery API"""
+    class Meta(object):
+        properties = ['id', 'url', 'date', 'cloud_score', 'resource']
 
-    def __init__(self, id='', url=None, date=None, cloud_score=None,
-                 resource=None):
-        super(EarthImagery, self).__init__()
-        self.id = id
-        self.url = url
-        self.date = date
-        self.cloud_score = cloud_score
-        self.resource = resource
+    def __init__(self, api, **kwargs):
+        super(EarthImagery, self).__init__(api, **kwargs)
         self._image = None
 
     def __repr__(self):
         return '<NasaEarthImagery id="%s">' % self.id
-
-    @classmethod
-    def query(cls, lat, lon, dim=None, date=None, cloud_score=None):
-        payload = {
-            'lat': lat, 'lon': lon, 'dim': dim, 'date': date,
-            'cloud_score': cloud_score,
-        }
-        filtered_payload = dict((k, v) for k, v in payload.iteritems() if v)
-        response = cls.api._get(
-            'https://api.data.gov/nasa/planetary/earth/imagery',
-            payload,
-        )
-        return EarthImagery.from_response(response)
-
-    @staticmethod
-    def from_response(result):
-        try:
-            score = result['cloud_score']
-        except KeyError:
-            score=None
-        return EarthImagery(
-            result['id'], result['url'], result['date'],
-            score, result['resource'],
-        )
 
     @property
     def image(self):
